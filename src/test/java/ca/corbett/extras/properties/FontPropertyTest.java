6d87b1b0d1a879445a73f39681242846f0cd9a78
Steve Corbett <steve@corbett.ca>
2025-08-23 21:57:00 -0600

    Issue #105 still in progress; adding unit tests

--- src/test/java/ca/corbett/extras/properties/FontPropertyTest.java
+++ src/test/java/ca/corbett/extras/properties/FontPropertyTest.java
@@ -2,13 +2,20 @@ package ca.corbett.extras.properties;
 
 import ca.corbett.forms.fields.FontField;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-class FontPropertyTest {
+class FontPropertyTest extends AbstractPropertyBaseTests {
+
+    @Override
+    protected AbstractProperty createTestObject(String name, String label) {
+        return new FontProperty(name, label);
+    }
 
     @Test
     public void generateFormField_withoutColors_shouldNotExposeColors() {
@@ -94,5 +101,21 @@ class FontPropertyTest {
         // THEN we should find our colors in there:
         assertEquals(Color.RED, actual.getTextColor());
         assertEquals(Color.YELLOW, actual.getBgColor());
+    }
+
+    @Test
+    public void formFieldChangeListener_withFormFieldChanges_shouldFireChangeEvents() {
+        // GIVEN a test prop with a mocked property form field change listener on it:
+        FontProperty testProp = new FontProperty("test", "test", new Font(Font.MONOSPACED, Font.PLAIN, 12));
+        PropertyFormFieldChangeListener listener = Mockito.mock(PropertyFormFieldChangeListener.class);
+        testProp.addFormFieldChangeListener(listener);
+
+        // WHEN we generate a form field and mess with it a bit:
+        FontField formField = (FontField)testProp.generateFormField();
+        formField.setSelectedFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+        formField.setSelectedFont(new Font(Font.SERIF, Font.BOLD, 14));
+
+        // THEN we should see our change listener got invoked:
+        Mockito.verify(listener, Mockito.times(2)).valueChanged(Mockito.any());
     }
 }
