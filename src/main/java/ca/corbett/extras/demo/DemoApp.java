462928061dc64863c0de60cc3950771d0bbeabf7
Steve Corbett <steve@corbett.ca>
2026-01-12 18:47:12 -0700

    Issue #249 - new visual effects classes for fade, blur, and snow

--- src/main/java/ca/corbett/extras/demo/DemoApp.java
+++ src/main/java/ca/corbett/extras/demo/DemoApp.java
@@ -3,6 +3,7 @@ package ca.corbett.extras.demo;
 import ca.corbett.extensions.demo.ExtensionsOverviewPanel;
 import ca.corbett.extras.Version;
 import ca.corbett.extras.demo.panels.AboutDemoPanel;
+import ca.corbett.extras.demo.panels.AnimationPanelEffectsPanel;
 import ca.corbett.extras.demo.panels.AnimationTextDemoPanel;
 import ca.corbett.extras.demo.panels.AudioDemoPanel;
 import ca.corbett.extras.demo.panels.DesktopDemoPanel;
@@ -110,6 +111,7 @@ public class DemoApp extends JFrame {
         addDemoPanel(new ImageUtilDemoPanel());
         addDemoPanel(new ImageTextUtilDemoPanel());
         addDemoPanel(new AnimationTextDemoPanel());
+        addDemoPanel(new AnimationPanelEffectsPanel()); // These are just for fun
         //addDemoPanel(new AnimationScrollDemoPanel()); // Not really a "swing extra"
         addDemoPanel(new ProgressDemoPanel());
         addDemoPanel(new PropertiesDemoPanel());
